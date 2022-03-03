7a06dc6bf7fc947525b170434bb119141952ba2a
Cale Surgen <680409+calebur@users.noreply.github.com>
2022-03-03 14:27:34 -0500

    Add PDP and timing to logging

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import com.chaos131.auto.AutoBuilder;
 import com.chaos131.auto.ParsedCommand;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -69,6 +70,7 @@ public class RobotContainer {
   private Intake m_intake = new Intake();
 
   private AutoBuilder m_autoBuilder = new AutoBuilder();
+  public PowerDistribution m_pdp = new PowerDistribution();
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
