043662bd8d224a290ba543f7435c49d133a52634
Joshua Allard <joshuaal2000@gmail.com>
2022-03-28 15:42:20 -0400

    Adjusts the SetSpeedLauncherShoot command to allow launching with the hood up

--- src/main/java/frc/robot/commands/SetSpeedLauncherShoot.java
+++ src/main/java/frc/robot/commands/SetSpeedLauncherShoot.java
@@ -13,16 +13,34 @@ import frc.robot.subsystems.Launcher;
 
 public class SetSpeedLauncherShoot extends BaseLauncherShoot {
   private double m_manualSpeed;
+  private boolean m_setHoodUp;
 
   /** Creates a new LauncherShoot. */
-  public SetSpeedLauncherShoot(Launcher launcher, Feeder feeder, double manualSpeed) {
+  public SetSpeedLauncherShoot(Launcher launcher, Feeder feeder, double manualSpeed, boolean setHoodUp) {
     super(launcher, feeder);
     m_manualSpeed = manualSpeed;
+    m_setHoodUp = setHoodUp;
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    if(m_setHoodUp) {
+      m_launcher.MoveHoodUp();
+    }
   }
   
   public static SetSpeedLauncherShoot CreateAutoCommand(ParsedCommand pc, Launcher launcher, Feeder feeder) {
     var speed = AutoUtil.parseDouble(pc.getArgument("speed"), Constants.DefaultLauncherLowSpeed);
-    return new SetSpeedLauncherShoot(launcher, feeder, speed);
+    return new SetSpeedLauncherShoot(launcher, feeder, speed, false);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    super.end(interrupted);
+    if(m_setHoodUp) {
+      m_launcher.MoveHoodDown();
+    }
   }
 
   @Override
