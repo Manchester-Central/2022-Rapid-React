50c29d7a49faf21d9a1e9bb02201ed15162b5fbf
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2023-11-30 19:38:50 -0500

    Began work on swerve by outline the flow of information from velocity to swerve states.

---
+++ src/main/java/subsystems/SwerveModule.java
@@ -0,0 +1,18 @@
+package subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+public class SwerveModule {
+    private TalonFX m_angle;
+    private TalonFX m_speed;
+    
+    public SwerveModule() {
+        
+    }
+
+    public void setState(SwerveModuleState state) {
+        
+    }
+}
